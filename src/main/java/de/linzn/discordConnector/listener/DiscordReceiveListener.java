06af84b206ab8833610a42dec756ec17d26938e6
Niklas <niklas.linz@enigmar.de>
2023-03-21 12:06:04 +0100

    use gpt-framework

--- src/main/java/de/linzn/discordConnector/listener/DiscordReceiveListener.java
+++ src/main/java/de/linzn/discordConnector/listener/DiscordReceiveListener.java
@@ -1,10 +1,16 @@
 package de.linzn.discordConnector.listener;
 
+import com.theokanning.openai.completion.chat.ChatMessage;
 import de.linzn.discordConnector.DiscordConnectorPlugin;
+import de.linzn.gptFramework.GPTFrameworkPlugin;
 import de.stem.stemSystem.STEMSystemApp;
+import de.stem.stemSystem.modules.pluginModule.STEMPlugin;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiscordReceiveListener extends ListenerAdapter {
     @Override
@@ -14,8 +20,13 @@ public class DiscordReceiveListener extends ListenerAdapter {
                 STEMSystemApp.LOGGER.CORE("Incoming chat from " + event.getAuthor().getName());
                 STEMSystemApp.LOGGER.CORE("ID " + event.getAuthor().getId());
                 STEMSystemApp.LOGGER.CORE("Value: " + event.getMessage().getContentDisplay());
-                DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToUser(event.getAuthor().getId(), "Hello " + event.getAuthor().getName() + ", i can not give you a valid answer. I think my openAI billing plan has no more free quota.");
 
+                //DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToUser(event.getAuthor().getId(), "Hello " + event.getAuthor().getName() + ", i can not give you a valid answer. I think my openAI billing plan has no more free quota.");
+
+                List<String> input = new ArrayList<>();
+                input.add(event.getMessage().getContentDisplay());
+                ChatMessage chatMessage = GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().getAIChatCompletion("Discord").requestCompletion(input);
+                DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToUser(event.getAuthor().getId(), chatMessage.getContent());
             }
         }
     }
