35d444ce0644d621ed58ac0d7a5eac5202de1811
Niklas <niklas.linz@enigmar.de>
2023-03-25 18:10:27 +0100

    update

--- src/main/java/de/linzn/discordConnector/DiscordManager.java
+++ src/main/java/de/linzn/discordConnector/DiscordManager.java
@@ -6,6 +6,13 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.nio.channels.Channel;
 
 public class DiscordManager {
     private final String token;
@@ -21,7 +28,7 @@ public class DiscordManager {
         JDABuilder jdaBuilder = JDABuilder.createLight(token);
         jdaBuilder.addEventListeners(new DiscordReceiveListener());
         jdaBuilder.setAutoReconnect(true);
-
+        jdaBuilder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
         this.jda = jdaBuilder.build();
         STEMSystemApp.LOGGER.CORE("Login to Discord API...");
         try {
@@ -42,5 +49,10 @@ public class DiscordManager {
     public void sendMessageToUser(String userId, String message) {
         User user = this.getJda().retrieveUserById(userId).complete();
         user.openPrivateChannel().complete().sendMessage(message).complete();
+    }
+
+    public void sendMessageToChannel(MessageChannel channel, String message) {
+
+        channel.sendMessage(message).complete();
     }
 }
--- src/main/java/de/linzn/discordConnector/listener/DiscordReceiveListener.java
+++ src/main/java/de/linzn/discordConnector/listener/DiscordReceiveListener.java
@@ -4,7 +4,9 @@ import de.linzn.discordConnector.DiscordConnectorPlugin;
 import de.linzn.gptFramework.GPTFrameworkPlugin;
 import de.stem.stemSystem.STEMSystemApp;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -12,45 +14,86 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 public class DiscordReceiveListener extends ListenerAdapter {
+    private final ConcurrentHashMap<String, Long> concurrentHashMapInstances;
 
-    private long date;
-
-    public DiscordReceiveListener(){
-        this.date= new Date().getTime();
+    public DiscordReceiveListener() {
+        concurrentHashMapInstances = new ConcurrentHashMap<>();
 
         STEMSystemApp.getInstance().getScheduler().runRepeatScheduler(DiscordConnectorPlugin.discordConnectorPlugin, () -> {
-            if(GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().hasAIChatCompletion("Discord")) {
-                if (this.date + Duration.ofMinutes(2).toMillis() < new Date().getTime()) {
-                    DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getPresence().setActivity(Activity.playing("Cleaning memory..."));
-                    STEMSystemApp.LOGGER.CORE("Destroying current instance of openAI! Timeout to prevent to much tokens!");
-                    GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().destroyAIChatCompletion("Discord");
-                    DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getPresence().setActivity(Activity.playing("Memory empty"));
+
+            for (String identityName : concurrentHashMapInstances.keySet()) {
+                long date = this.concurrentHashMapInstances.get(identityName);
+                if (date + Duration.ofMinutes(2).toMillis() < new Date().getTime()) {
+                    STEMSystemApp.LOGGER.CORE("Destroying instance from " + identityName + " of openAI! Timeout to prevent to much tokens!");
+                    GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().destroyAIChatCompletion(DiscordConnectorPlugin.discordConnectorPlugin, identityName);
+                    this.concurrentHashMapInstances.remove(identityName);
                 }
             }
+            DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getPresence().setActivity(Activity.playing("Instances: " + this.concurrentHashMapInstances.size()));
         }, 4, 1, TimeUnit.SECONDS);
     }
+
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.isFromType(ChannelType.PRIVATE)) {
             if (event.getAuthor() != DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getSelfUser()) {
-                STEMSystemApp.LOGGER.CORE("Incoming chat from " + event.getAuthor().getName());
-                STEMSystemApp.LOGGER.CORE("ID " + event.getAuthor().getId());
-                STEMSystemApp.LOGGER.CORE("Value: " + event.getMessage().getContentDisplay());
+                String inputData = event.getMessage().getContentDisplay();
+                User user = event.getAuthor();
 
-                STEMSystemApp.getInstance().getScheduler().runTask(DiscordConnectorPlugin.discordConnectorPlugin, () -> {
-                    this.date = new Date().getTime();
-                    DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getPresence().setActivity(Activity.playing("Working on GPT result..."));
-                    List<String> input = new ArrayList<>();
-                    input.add(event.getMessage().getContentDisplay());
-                    String chatMessage = GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().getAIChatCompletion("Discord").requestCompletion(input);
-                    STEMSystemApp.LOGGER.CORE("Receiving output...");
-                    DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToUser(event.getAuthor().getId(), chatMessage);
-                    DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getPresence().setActivity(Activity.playing("Instance in Memory"));
-                });
+                STEMSystemApp.LOGGER.INFO("Incoming communication from " + user.getName());
+                STEMSystemApp.LOGGER.INFO("Input: " + inputData);
+
+                this.processGPT(user.getName(), inputData, event.getChannel());
+
+            }
+        } else {
+            if (event.getAuthor() != DiscordConnectorPlugin.discordConnectorPlugin.discordManager.getJda().getSelfUser()) {
+                if (event.getChannel().getId().equalsIgnoreCase("1088490251116359750")) {
+                    String inputData = event.getMessage().getContentDisplay();
+                    User user = event.getAuthor();
+
+                    STEMSystemApp.LOGGER.INFO("Incoming communication channel from " + user.getName());
+                    STEMSystemApp.LOGGER.INFO("Input: " + inputData);
+
+                    this.processGPT(user.getName(), inputData, event.getChannel());
+                }
             }
         }
+    }
+
+    private void processGPT(String sender, String content, MessageChannel channel) {
+
+        if (content.startsWith("CreateImage: ")) {
+            STEMSystemApp.LOGGER.INFO("Image request...");
+            String url = GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().createAIImageCompletion().requestCompletion(content.replace("CreateImage:", ""));
+            STEMSystemApp.LOGGER.INFO("Callback received!");
+            DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToChannel(channel, url);
+
+        } else {
+            STEMSystemApp.LOGGER.INFO("Chat request...");
+            List<String> input = new ArrayList<>();
+            input.add("[" + sender + "]-> " + content);
+
+            setInstance(channel.getId(), new Date().getTime());
+            String chatMessage = GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().createAIChatCompletion(DiscordConnectorPlugin.discordConnectorPlugin, channel.getId()).requestCompletion(input);
+            STEMSystemApp.LOGGER.INFO("Callback received!");
+            DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToChannel(channel, chatMessage);
+
+            if (chatMessage.startsWith("An error was catch")) {
+                input = new ArrayList<>();
+                input.add("An error was generated by you. Please inform me: " + chatMessage);
+                chatMessage = GPTFrameworkPlugin.gptFrameworkPlugin.getGptManager().createAIChatCompletion(DiscordConnectorPlugin.discordConnectorPlugin, channel.getId()).requestCompletion(input);
+                DiscordConnectorPlugin.discordConnectorPlugin.discordManager.sendMessageToChannel(channel, chatMessage);
+            }
+        }
+    }
+
+    private void setInstance(String identity, long date) {
+        this.concurrentHashMapInstances.put(identity, date);
     }
 }
